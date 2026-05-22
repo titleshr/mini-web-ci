453318b7c4aac27153c696928ce095e269aedcc7
Saharat Nathi <saharat.tester@myorder.ai>
2026-05-22 11:41:50 +0700

    add docker component test with mountebank

--- src/order/order.controller.spec.ts
+++ src/order/order.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
 
 describe('OrderController', () => {
   let controller: OrderController;
 
+  const mockOrderService = {
+    createOrder: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
+      providers: [
+        {
+          provide: OrderService,
+          useValue: mockOrderService,
+        },
+      ],
     }).compile();
 
     controller = module.get<OrderController>(OrderController);
